cb695a7616b76ebdb18ad7a4fdca90da139a7357
Наталья Нуйкина <me@nuikina.ru>
2024-01-22 18:36:46 +0300

    update brain-games.js and cli.js after eslint

--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,6 @@
 import readlineSync from 'readline-sync';
-  export const name = readlineSync.question('Welcome to the Brain Games! \nMay I have your name? ');
-  console.log(`Hello, ${name}!`);
+
+export const name = readlineSync.question('Welcome to the Brain Games! \nMay I have your name? ');
+console.log(`Hello, ${name}!`);
+
+export default name;
