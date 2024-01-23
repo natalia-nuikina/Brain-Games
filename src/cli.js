a8d22e4c7239f8bbb51d0af8fbe23f658484c65c
Наталья Нуйкина <me@nuikina.ru>
2024-01-23 11:15:25 +0300

    update const name to userName

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-export const name = readlineSync.question('Welcome to the Brain Games! \nMay I have your name? ');
-console.log(`Hello, ${name}!`);
+const userName = readlineSync.question('Welcome to the Brain Games! \nMay I have your name? ');
+console.log(`Hello, ${userName}!`);
 
-export default name;
+export default userName;
