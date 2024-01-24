5c3212f79bea976338fa2d9241b3ae342eae5c51
Наталья Нуйкина <me@nuikina.ru>
2024-01-24 23:59:26 +0300

    update brain-even after index.js

--- src/even.js
+++ src/even.js
@@ -1,5 +1,5 @@
-import readlineSync from 'readline-sync';
 import userName from './cli.js';
+import workWithUser from './index.js';
 
 const brainGame = () => {
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
@@ -7,13 +7,10 @@ const brainGame = () => {
   let correctAnswer;
   for (let i = 0; i < 3; i += 1) {
     const roundNumber = Math.round(Math.random() * toGetWholeNum);
-    console.log(`Question: ${roundNumber}`);
-    const userAnswer = readlineSync.question('Your answer: ');
     correctAnswer = (roundNumber % 2 === 0) ? 'yes' : 'no';
-    if (userAnswer === correctAnswer) {
-      console.log('Correct!');
-    } else {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
+    const question = roundNumber;
+    const isCorrect = workWithUser(question, correctAnswer);
+    if (isCorrect === 0) {
       return;
     }
   }
