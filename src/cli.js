1db2ea672fa4e2039c630d3c5aaf3440fb9ce4d4
Наталья Нуйкина <me@nuikina.ru>
2024-01-26 22:06:01 +0300

    fixed errors pointed out by linter

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
 
-const userName = readlineSync.question('Welcome to the Brain Games! \nMay I have your name? ');
-console.log(`Hello, ${userName}!`);
+const userName = () => {
+  const name = readlineSync.question('Welcome to the Brain Games! \nMay I have your name? ');
+  console.log(`Hello, ${name}!`);
+};
 
 export default userName;
