fa47d672bb25e2443fc9e7a315faeb280611dedf
Наталья Нуйкина <me@nuikina.ru>
2024-01-29 23:05:08 +0300

    Скорректировала функцию isEven в игре prime

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,7 +1,17 @@
 import getRandomNum from '../helpers.js';
 import runEngine from '../index.js';
 
-const isEven = (number) => number % 2 === 0;
+const isEven = (number) => {
+  if (number < 2) {
+    return false;
+  }
+  for (let i = 2; i < number / 2; i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
 
 export const task = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
